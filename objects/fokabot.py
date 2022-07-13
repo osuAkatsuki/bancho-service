012da8e5acc07bc290a3585a5804923856e34a0d
cmyui <cmyuiosu@gmail.com>
2022-07-13 00:24:18 -0400

    simplify & cleanup fokabot command handling

--- objects/fokabot.py
+++ objects/fokabot.py
@@ -2,7 +2,7 @@ import re
 import threading
 from queue import Queue
 from time import time
-from typing import Union
+from typing import Optional
 
 from common.constants import actions
 from common.ripple import userUtils
@@ -10,14 +10,15 @@ from constants import fokabotCommands, serverPackets
 from objects import glob
 
 # Some common regexes, compiled to increase performance.
-reportRegex = re.compile(r'^(.+) \((.+)\)\:(?: )?(.+)?$')
-usernameRegex = re.compile(r'^[\w \[\]-]{2,15}$')
+reportRegex = re.compile(r"^(.+) \((.+)\)\:(?: )?(.+)?$")
+usernameRegex = re.compile(r"^[\w \[\]-]{2,15}$")
 npRegex = re.compile(
-    r'^https?://osu\.(?:akatsuki\.pw|akatest\.space|ppy\.sh)/beatmapsets/'
-    r'(?P<set_id>\d{1,10})/?#/?'
-    r'(?P<mode>osu|taiko|fruits|mania)?/?'
-    r'(?P<id>\d{1,10})/?$'
+    r"^https?://osu\.(?:akatsuki\.pw|akatest\.space|ppy\.sh)/beatmapsets/"
+    r"(?P<set_id>\d{1,10})/?#/?"
+    r"(?P<mode>osu|taiko|fruits|mania)?/?"
+    r"(?P<id>\d{1,10})/?$"
 )
+
 
 def connect() -> None:
     with glob.tokens:
@@ -27,17 +28,29 @@ def connect() -> None:
     glob.streams.broadcast("main", serverPackets.userPanel(999))
     glob.streams.broadcast("main", serverPackets.userStats(999))
 
+
 def disconnect() -> None:
     with glob.tokens:
-        glob.tokens.deleteToken(glob.tokens.getTokenFromUserID(999))
+        token = glob.tokens.getTokenFromUserID(999)
+        assert token is not None
+
+        glob.tokens.deleteToken(token)
+
 
 # def reload_commands():
 # 	"""Reloads the Fokabot commands module."""
 #     # NOTE: this is not safe, will break references
 # 	reload(fokabotCommands)
 
+from typing import TypedDict
 
-def fokabotResponse(fro: str, chan: str, message: str) -> Union[str, bool]:
+
+class CommandResponse(TypedDict):
+    response: str
+    hidden: bool
+
+
+def fokabotResponse(fro: str, chan: str, message: str) -> Optional[CommandResponse]:
     """
     Check if a message has triggered FokaBot
 
@@ -59,28 +72,25 @@ def fokabotResponse(fro: str, chan: str, message: str) -> Union[str, bool]:
 
         # Make sure the user has right permissions
         if (
-            cmd['privileges'] and
-            not userUtils.getPrivileges(userID) & cmd['privileges']
+            cmd["privileges"]
+            and not userUtils.getPrivileges(userID) & cmd["privileges"]
         ):
-            return False
+            return None
 
         # If this is an !mp command in a match, make sure the user is a referee.
-        if (
-            chan.startswith('#multi_') and
-            cmd['trigger'] == '!mp' and
-            not glob.matches.getMatchFromChannel(chan).is_referee(userID)
-        ):
-            return False
+        if chan.startswith("#multi_") and cmd["trigger"] == "!mp":
+            match = glob.matches.getMatchFromChannel(chan)
+            assert match is not None
+
+            if not match.is_referee(userID):
+                return None
 
         # Check argument number
-        message_split = message.split(' ')
-        if (
-            cmd['syntax'] and
-            len(message_split) <= cmd['syntax'].count(' ') + 1
-        ):
+        message_split = message.split(" ")
+        if cmd["syntax"] and len(message_split) <= cmd["syntax"].count(" ") + 1:
             return {
-                'response': f'Incorrect syntax: {cmd["trigger"]} {cmd["syntax"]}',
-                'hidden': True
+                "response": f'Incorrect syntax: {cmd["trigger"]} {cmd["syntax"]}',
+                "hidden": True,
             }
 
         def handle_command(cmd, fro, chan, msg):
@@ -91,10 +101,12 @@ def fokabotResponse(fro: str, chan: str, message: str) -> Union[str, bool]:
 
             return resp
 
-        if cmd['callback']:
+        if cmd["callback"]:
             queue = Queue()
             thread = threading.Thread(
-                target=lambda q, arg1, arg2, arg3: q.put(handle_command(cmd, arg1, arg2, arg3)),
+                target=lambda q, arg1, arg2, arg3: q.put(
+                    handle_command(cmd, arg1, arg2, arg3)
+                ),
                 args=(queue, fro, chan, message_split[1:]),
                 daemon=True,
             )
@@ -106,19 +118,16 @@ def fokabotResponse(fro: str, chan: str, message: str) -> Union[str, bool]:
             if isinstance(resp, Exception):
                 raise resp
         else:
-            resp = cmd['response']
+            resp = cmd["response"]
 
         if not resp:
-            return False
+            return None
 
         resp = [resp]
         if userID == 1001:
-            resp.append(f'Elapsed: {(time() - start_time) * 1000:.3f}ms')
+            resp.append(f"Elapsed: {(time() - start_time) * 1000:.3f}ms")
 
-        return {
-            'response': ' | '.join(resp),
-            'hidden': cmd['hidden']
-        }
+        return {"response": " | ".join(resp), "hidden": cmd["hidden"]}
 
     # No commands triggered
-    return False
+    return None
