34caaf1bcdadca4e4b21e15b8b4814bb207381d0
osu!Akatsuki Developer <support@akatsuki.pw>
2022-08-16 16:51:30 +0000

    remove unused imports, fix logic order of protocol packet

--- events/changeProtocolVersionEvent.py
+++ events/changeProtocolVersionEvent.py
@@ -1,13 +1,11 @@
-from common.constants import actions, mods
-from common.ripple import userUtils
-from constants import clientPackets, serverPackets
-from objects import glob
+from constants import clientPackets
 
 from common.log import logUtils as log
 
 def handle(userToken, packetData):
     """ User is using Akatsuki's patcher and is trying to upgrade their connection. """
 
-    log.info(f"{userToken.username} upgrading connection to protocol v{userToken.protocolVersion}.")
     packetData = clientPackets.changeProtocolVersion(packetData)
     userToken.protocolVersion = packetData["version"]
+
+    log.info(f"{userToken.username} upgraded connection to protocol v{userToken.protocolVersion}.")
