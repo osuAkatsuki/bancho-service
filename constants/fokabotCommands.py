034ceb68813cae7988bb64183826595b1e3f4115
cmyui <cmyuiosu@gmail.com>
2023-07-16 13:53:25 -0400

    also track match start from !mp command

--- constants/fokabotCommands.py
+++ constants/fokabotCommands.py
@@ -12,6 +12,8 @@ from typing import Optional
 import orjson
 import requests
 
+from uuid import uuid4
+from amplitude import BaseEvent
 import settings
 from common import generalUtils
 from common.constants import gameModes
@@ -1998,6 +2000,38 @@ def multiplayer(fro: str, chan: str, message: list[str]) -> Optional[str]:
                     to=chan,
                     message="Have fun!",
                 )
+
+                insert_id = str(uuid4())
+                glob.amplitude.track(
+                    BaseEvent(
+                        event_type="start_multiplayer_match",
+                        user_id=str(userID),
+                        event_properties={
+                            "match": {
+                                "match_id": multiplayer_match["match_id"],
+                                "match_name": multiplayer_match["match_name"],
+                                # "match_password": multiplayer_match["match_password"],
+                                "beatmap_id": multiplayer_match["beatmap_id"],
+                                "beatmap_name": multiplayer_match["beatmap_name"],
+                                "beatmap_md5": multiplayer_match["beatmap_md5"],
+                                "game_mode": multiplayer_match["game_mode"],
+                                "host_user_id": multiplayer_match["host_user_id"],
+                                "mods": multiplayer_match["mods"],
+                                "match_scoring_type": multiplayer_match["match_scoring_type"],
+                                "match_team_type": multiplayer_match["match_team_type"],
+                                "match_mod_mode": multiplayer_match["match_mod_mode"],
+                                "seed": multiplayer_match["seed"],
+                                "is_tourney": multiplayer_match["is_tourney"],
+                                "is_locked": multiplayer_match["is_locked"],
+                                "is_starting": multiplayer_match["is_starting"],
+                                "is_in_progress": multiplayer_match["is_in_progress"],
+                                "creation_time": multiplayer_match["creation_time"],
+                            },
+                        },
+                        insert_id=insert_id,
+                    )
+                )
+
                 return False
 
         def _decreaseTimer(t: int) -> None:
