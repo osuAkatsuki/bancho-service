dec91c3e603ac16183cd3b685b7d6a463f9f22d4
cmyui <cmyuiosu@gmail.com>
2023-09-17 17:32:03 -0400

    more flexibility for lock api

--- objects/redisLock.py
+++ objects/redisLock.py
@@ -1,24 +1,27 @@
 from __future__ import annotations
 
 import time
-from typing import Optional
 
 from objects import glob
 
-LOCK_EXPIRY = 10  # in seconds
-RETRY_DELAY = 0.05  # in seconds
+DEFAULT_LOCK_EXPIRY = 10  # in seconds
+DEFAULT_RETRY_DELAY = 0.05  # in seconds
 
 
 class redisLock:
     def __init__(self, key: str) -> None:
         self.key = key
 
-    def try_acquire(self) -> (bool | None):
-        return glob.redis.set(self.key, "1", ex=LOCK_EXPIRY, nx=True)
+    def _try_acquire(self, expiry: float) -> bool | None:
+        return glob.redis.set(self.key, "1", ex=expiry, nx=True)
 
-    def acquire(self) -> None:
-        while not self.try_acquire():
-            time.sleep(RETRY_DELAY)
+    def acquire(
+        self,
+        expiry: float = DEFAULT_LOCK_EXPIRY,
+        retry_delay: float = DEFAULT_RETRY_DELAY,
+    ) -> None:
+        while not self._try_acquire(expiry):
+            time.sleep(retry_delay)
 
     def release(self) -> None:
         glob.redis.delete(self.key)
