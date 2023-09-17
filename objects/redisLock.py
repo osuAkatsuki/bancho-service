3d76d7e734c6038e7d1c2ca3a269dcb6a7ca05fb
infernalfire72 <jkoe2502@gmail.com>
2023-09-17 15:08:26 +0200

    timeout locks

--- objects/redisLock.py
+++ objects/redisLock.py
@@ -5,27 +5,19 @@ from typing import Optional
 
 from objects import glob
 
+LOCK_EXPIRY = 10  # in seconds
+RETRY_DELAY = 0.05  # in seconds
 
 class redisLock:
     def __init__(self, key: str) -> None:
         self.key = key
 
-    def is_locked(self) -> bool:
-        is_locked = False
-
-        lock_bytes: Optional[bytes] = glob.redis.get(self.key)
-        if lock_bytes is not None:
-            is_locked = lock_bytes.decode() == "1"
-
-        return is_locked
-
-    def wait_for_release(self) -> None:
-        while self.is_locked():
-            time.sleep(0.01)
+    def try_acquire(self) -> (bool | None):
+        return glob.redis.set(self.key, "1", ex=LOCK_EXPIRY, nx=True)
 
     def acquire(self) -> None:
-        self.wait_for_release()
-        glob.redis.set(self.key, "1")
+        while not self.try_acquire():
+            time.sleep(RETRY_DELAY)
 
     def release(self) -> None:
         glob.redis.delete(self.key)
