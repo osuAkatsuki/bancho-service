ef6e9b4d885d91952bac40a9b669268c180fe5e5
cmyui <cmyuiosu@gmail.com>
2023-10-08 03:37:20 -0400

    tempfix

--- scripts/inject-vault-secrets-into-env.sh
+++ scripts/inject-vault-secrets-into-env.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-pip install akatsuki-cli
+PYPI_INDEX_URL="http://localhost:3141/cmyui/dev"
+
+pip install -i $PYPI_INDEX_URL akatsuki-cli
 akatsuki vault get bancho-service ${APP_ENV} -o .env
 
 set -a
