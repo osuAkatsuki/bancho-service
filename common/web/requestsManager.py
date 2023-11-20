07cbdb7d033ab380b61787af5167a9ecc83233cd
cmyui <cmyuiosu@gmail.com>
2023-11-19 21:55:14 -0500

    (distributed) request id tracing

--- common/web/requestsManager.py
+++ common/web/requestsManager.py
@@ -1,10 +1,18 @@
 from __future__ import annotations
 
+from uuid import uuid4
+
 import tornado.web
+
+from common.log import logger
 
 
 class AsyncRequestHandler(tornado.web.RequestHandler):
     """A thin wrapper around tornado.web.RequestHandler to add some useful methods."""
+
+    def prepare(self) -> None:
+        request_id = self.request.headers.get("X-Request-ID", None) or str(uuid4())
+        logger.add_context(request_id=request_id)
 
     def getRequestIP(self):
         if "CF-Connecting-IP" in self.request.headers:
