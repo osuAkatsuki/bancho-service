e80f809343d906b6a9802112677bf7ed97248dbb
cmyui <cmyuiosu@gmail.com>
2024-05-12 04:02:47 -0400

    lint fix

--- irc/ircserver.py
+++ irc/ircserver.py
@@ -6,7 +6,6 @@ by Joel Rosdahl, licensed under the GNU GPL 2 License.
 Most of the reference code from miniircd was used for the low-level logic.
 The high-level code has been rewritten to make it compatible with bancho-service.
 """
-
 # NOTE(2023-08-10): this is currently deprecated within akatsuki and is not functional
 # here because we may use it in the future - perhaps for deletion eventually.
 from __future__ import annotations
