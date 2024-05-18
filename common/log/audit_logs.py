18c57482d6fa65ca68403362393e0b2fe1ee63a5
cmyui <cmyuiosu@gmail.com>
2024-05-18 05:15:06 -0400

    remove unnecessary nesting

--- common/log/audit_logs.py
+++ common/log/audit_logs.py
@@ -24,42 +24,41 @@ DISCORD_WEBHOOK_EMBED_COLOR = 0x7352C4
 async def send_log_as_discord_webhook(message: str, discord_channel: str) -> None:
     """Log a message to the provided discord channel, if configured."""
 
-    if discord_channel is not None:
-        discord_webhook_url = DISCORD_CHANNELS.get(discord_channel)
-        if discord_webhook_url is None:
-            logger.error(
-                "Attempted to send webhook to an unknown discord channel",
-                extra={"discord_channel": discord_channel},
-            )
-            return
-        elif discord_webhook_url == "":
-            logger.warning(
-                f"No discord webhook embed is configurated for discord channel",
-                extra={"discord_channel": discord_channel},
-            )
-            return
-
-        embed = Webhook(discord_webhook_url, color=DISCORD_WEBHOOK_EMBED_COLOR)
-        embed.set_author(
-            name="",
-            icon="",
+    discord_webhook_url = DISCORD_CHANNELS.get(discord_channel)
+    if discord_webhook_url is None:
+        logger.error(
+            "Attempted to send webhook to an unknown discord channel",
+            extra={"discord_channel": discord_channel},
         )
-        embed.add_field(name="New moderation action logged! :tools:", value=message)
-        embed.set_footer(text="bancho-service ⚓")
+        return
+    elif discord_webhook_url == "":
+        logger.warning(
+            "No discord webhook embed is configurated for discord channel",
+            extra={"discord_channel": discord_channel},
+        )
+        return
 
-        for _ in range(MAX_RETRIES):
-            try:
-                await embed.post()
-                break
-            except (httpx.NetworkError, httpx.TimeoutException):
+    embed = Webhook(discord_webhook_url, color=DISCORD_WEBHOOK_EMBED_COLOR)
+    embed.set_author(
+        name="",
+        icon="",
+    )
+    embed.add_field(name="New moderation action logged! :tools:", value=message)
+    embed.set_footer(text="bancho-service ⚓")
+
+    for _ in range(MAX_RETRIES):
+        try:
+            await embed.post()
+            break
+        except (httpx.NetworkError, httpx.TimeoutException):
+            await asyncio.sleep(RETRY_INTERVAL)
+            continue
+        except httpx.HTTPStatusError as exc:
+            if exc.response.status_code == 429:
                 await asyncio.sleep(RETRY_INTERVAL)
                 continue
-            except httpx.HTTPStatusError as exc:
-                if exc.response.status_code == 429:
-                    await asyncio.sleep(RETRY_INTERVAL)
-                    continue
-                else:
-                    raise
+            else:
+                raise
 
 
 async def send_log(
