589b2d16966663b400fbba719f1a1a88fc49dbcc
cmyui <cmyuiosu@gmail.com>
2024-07-02 02:58:09 -0400

    type annotate

--- common/tracing_utils.py
+++ common/tracing_utils.py
@@ -3,10 +3,16 @@ from __future__ import annotations
 import logging
 import os.path
 import sys
+from collections.abc import Callable
+from typing import ParamSpec
+from typing import TypeVar
+
+P = ParamSpec("P")
+R = TypeVar("R")
 
 
-def tracef(f):
-    def tracef(*args, **kwargs):
+def tracef(f: Callable[P, R]) -> Callable[P, R]:
+    def tracef(*args: P.args, **kwargs: P.kwargs) -> R:
         # print where this is being called from
         caller_frame = sys._getframe(1)
         logging.info(
