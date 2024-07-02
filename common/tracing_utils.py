960df61ea5195cd60ec49f1f69d840a2dc4e4d83
cmyui <cmyuiosu@gmail.com>
2024-07-02 03:26:23 -0400

    coro timing for tracef

--- common/tracing_utils.py
+++ common/tracing_utils.py
@@ -4,6 +4,8 @@ import functools
 import logging
 import os.path
 import sys
+import time
+from collections.abc import Awaitable
 from collections.abc import Callable
 from typing import ParamSpec
 from typing import TypeVar
@@ -12,20 +14,30 @@ P = ParamSpec("P")
 R = TypeVar("R")
 
 
-def tracef(f: Callable[P, R]) -> Callable[P, R]:
+def tracef(f: Callable[P, Awaitable[R]]) -> Callable[P, Awaitable[R]]:
     """A helper to understand where functions are being called."""
 
     @functools.wraps(f)
-    def tracef(*args: P.args, **kwargs: P.kwargs) -> R:
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> Awaitable[R]:
         caller_frame = sys._getframe(1)
-        logging.info(
-            "Traced function",
-            extra={
-                "function": f.__name__,
-                "file": os.path.basename(caller_frame.f_code.co_filename),
-                "line": caller_frame.f_lineno,
-            },
-        )
-        return f(*args, **kwargs)
 
-    return tracef
+        async def async_wrapper() -> R:
+            st = time.time()
+            value = await f(*args, **kwargs)
+            et = time.time()
+
+            file = os.path.basename(caller_frame.f_code.co_filename)
+            line = caller_frame.f_lineno
+            logging.info(
+                "Traced function execution",
+                extra={
+                    "function": f.__name__,
+                    "execution_time_ms": (et - st) * 1000,
+                    "code_location": f"{file}:{line}",
+                },
+            )
+            return value
+
+        return async_wrapper()
+
+    return wrapper
