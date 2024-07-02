788ec4d61b0ddba8ea0162b334409764d4fed13c
cmyui <cmyuiosu@gmail.com>
2024-07-02 03:44:11 -0400

    second and third layer code location

--- common/tracing_utils.py
+++ common/tracing_utils.py
@@ -20,6 +20,8 @@ def tracef(f: Callable[P, Awaitable[R]]) -> Callable[P, Awaitable[R]]:
     @functools.wraps(f)
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> Awaitable[R]:
         caller_frame = sys._getframe(1)
+        second_caller_frame = sys._getframe(2)
+        third_caller_frame = sys._getframe(3)
 
         async def async_wrapper() -> R:
             st = time.time()
@@ -28,12 +30,21 @@ def tracef(f: Callable[P, Awaitable[R]]) -> Callable[P, Awaitable[R]]:
 
             file = os.path.basename(caller_frame.f_code.co_filename)
             line = caller_frame.f_lineno
+
+            second_file = os.path.basename(second_caller_frame.f_code.co_filename)
+            second_line = second_caller_frame.f_lineno
+
+            third_file = os.path.basename(third_caller_frame.f_code.co_filename)
+            third_line = third_caller_frame.f_lineno
+
             logging.info(
                 "Traced function execution",
                 extra={
                     "function": f.__name__,
                     "execution_time_ms": (et - st) * 1000,
                     "code_location": f"{file}:{line}",
+                    "second_code_location": f"{second_file}:{second_line}",
+                    "third_code_location": f"{third_file}:{third_line}",
                 },
             )
             return value
