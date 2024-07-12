33d6592f4ea539edc39c22afa252bca4e5455012
Mahmood <shadowofextinction@gmail.com>
2024-07-13 00:54:26 +0100

    addbn command

--- constants/chatbotCommands.py
+++ constants/chatbotCommands.py
@@ -128,6 +128,20 @@ async def _help(fro: str, chan: str, message: list[str]) -> str:
 
     return "\n".join(l)
 
+@command(trigger="!addbn",
+         syntax="<name>" ,
+         privs=privileges.ADMIN_MANAGE_PRIVILEGES,
+         )
+async def addbn(fro: str, chan: str, message: list[str]) -> str:
+    """Add BN priveleges to a user"""
+    userid = await user_utils.get_id_from_username(fro)
+    if not (targetID := await user_utils.get_id_from_username(userid)):
+        return "Could not find user"
+    current_priveleges = await user_utils.get_privileges(userid)
+    new_priveleges = current_priveleges | 256
+    
+    user_utils.set_privileges(userid,new_priveleges)
+    
 
 @command(trigger="!faq", syntax="<name>")
 async def faq(fro: str, chan: str, message: list[str]) -> str:
