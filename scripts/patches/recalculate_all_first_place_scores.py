a9b1be44c696636119bdf2d01d7e8bcbfa9ee319
cmyui <cmyuiosu@gmail.com>
2025-06-30 23:56:11 -0400

    patch script for first places

---
+++ scripts/patches/recalculate_all_first_place_scores.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+
+import asyncio
+
+import lifecycle
+from common.ripple import user_utils
+from objects import glob
+
+
+async def main() -> int:
+    await lifecycle.startup()
+    users = await glob.db.fetchAll("SELECT id FROM users WHERE privileges & 3 = 3")
+    for user in users:
+        await user_utils.recalculate_and_update_first_place_scores(user["id"])
+    await lifecycle.shutdown()
+    return 0
+
+
+if __name__ == "__main__":
+    exit(asyncio.run(main()))
