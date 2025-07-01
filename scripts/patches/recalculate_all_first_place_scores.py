7795df24fcf704a05ac16670b9225425bce16814
Josh Smith <cmyuiosu@gmail.com>
2025-07-01 00:02:49 -0400

    First places fix script (#199)
    * patch script for first places
    
    * log

---
+++ scripts/patches/recalculate_all_first_place_scores.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+
+import asyncio
+import logging
+
+import lifecycle
+from common.ripple import user_utils
+from objects import glob
+
+
+async def main() -> int:
+    await lifecycle.startup()
+    users = await glob.db.fetchAll("SELECT id FROM users WHERE privileges & 3 = 3")
+    for user in users:
+        await user_utils.recalculate_and_update_first_place_scores(user["id"])
+        logging.info("Recalculated first place scores for user %d", user["id"])
+    await lifecycle.shutdown()
+    return 0
+
+
+if __name__ == "__main__":
+    exit(asyncio.run(main()))
